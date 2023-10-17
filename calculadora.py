e321052d5582a3a8d37a2c207b4e1307e3cdc74f
Gemma <gemma.garrigosa@gmail.com>
2023-10-17 19:19:03 +0200

    Afegits fitxers calculadora

---
+++ calculadora.py
@@ -0,0 +1,19 @@
+
+"""
+Calculadora basica
+"""
+
+__author__ = "Xavi"
+
+class Calculadora():
+    def suma(self, a, b):
+        return a+b
+
+    def resta(self, a, b):
+        return a-b
+
+    def multiplica(self, a, b):
+        return a*b
+
+    def divisio(self, a, b):
+        return a/b
